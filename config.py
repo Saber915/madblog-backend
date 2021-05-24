464f1f85ddcad50cb2a24aa30ec84db195c6c430
陈良军 liangjun.chen <liangjun.chen@linkedcare.cn>
2021-05-24 16:32:39 +0800

    3. Flask 设计User用户相关API

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv(os.path.join(basedir, '.env'), encoding='utf-8')
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/madblog?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/madblog?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
